547ad81b1f554a352b5908393ce8ae93f6dccf8e
Joseph Reed <jereed559@gmail.com>
2020-02-26 16:45:45 -0800

    Initial Author routes

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-const indexRouter = require('./routes/index.js');
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');  
+const indexRouter = require('./routes/index.js');
+const authorRouter = require('./routes/authors');
 
 // Connect to mongoose
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
   .then(() => console.log('MongoDB connected...'))
+  .catch(error => console.log(error))
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.set('layouts', '/layouts/layout');
+app.set('layout', 'layout.ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false })) 
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(PORT);
+
