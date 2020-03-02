371a070c16de2be161d421febfaa0cd9660e88e2
Joseph Reed <jereed559@gmail.com>
2020-03-02 00:21:06 -0800

    Finish Book Routes

--- models/author.js
+++ models/author.js
@@ -9,7 +9,7 @@ const authorSchema = new mongoose.Schema({
 })
 
 authorSchema.pre('remove', function(next) {
-  Book.find({ author: this.id}, (err, books) => {
+  Book.find({ author: this.id }, (err, books) => {
     if (err) {
       next(err)
     } else if (books.length > 0) {
